a2ab82792af01e1d83728203b882fe4e69b7a077
Iván Vargas O <ivanvargas.va@gmail.com>
2025-12-04 03:35:22 -0500

    feat: First version

---
+++ main.js
@@ -0,0 +1,55 @@
+const tracks = [
+    {
+        name: 'Ya no aguanto más',
+        sound: 'data/sounds/ya-no-aguanto-mas.mp3',
+        thumb: 'img/thumb-track.svg',
+    },
+    {
+        name: 'Dios mio sálvame',
+        sound: 'data/sounds/dios-mio-salvame.mp3',
+        thumb: 'img/thumb-track.svg',
+    },
+    {
+        name: 'Que vivan los mongólicos',
+        sound: 'data/sounds/que-vivan-los-mongolicos-diomedes-diaz',
+        thumb: 'img/thumb-track.svg',
+    },
+    {
+        name: 'Maricaaa',
+        sound: 'data/sounds/maricaa.mp3',
+        thumb: 'img/thumb-track.svg',
+    },
+    {
+        name: 'Quedé-mínimo-común',
+        sound: 'data/sounds/quede-minimo-comun.mp3',
+        thumb: 'img/thumb-track.svg',
+    },
+    {
+        name: 'Señor me has mirado a los ojos',
+        sound: 'data/sounds/senor-me-has-mirado-a-los-ojos.mp3',
+        thumb: 'img/thumb-track.svg',
+    },
+    {
+        name: 'Feliz navidad inmundo animal',
+        sound: 'data/sounds/feliz-navidad-inmundo-animal.mp3',
+        thumb: 'img/thumb-track.svg',
+    },
+];
+
+const container_tracks = document.getElementById('container-tracks');
+
+tracks.forEach(track => {
+    const newTrack = document.createElement('article');
+    newTrack.className = 'track';
+    newTrack.innerHTML = `
+        <div class="track__thumb-container">
+            <img src="${track.thumb}" alt="thumb track" class="track__thumb">
+        </div>
+        <h2 class="track__name">${track.name}</h2>
+    `;
+    newTrack.addEventListener('click' , ()=> {
+        const sonido = new Audio(track.sound);
+        sonido.play();
+    })
+    container_tracks.appendChild(newTrack);
+});
